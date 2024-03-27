6f647184abd08b232dd52bbaa790912a67e7ea9a
Noriko30402 <151799807+Noriko30402@users.noreply.github.com>
2024-03-27 14:52:03 +0900

    Update event.js

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,5 +1,6 @@
 
 const text=document.getElementById('text')
+const btn=document.getElementById("btn")
 btn.addEventListener('click',()=>{
   setTimeout(()=>{
       text.textContent='ボタンがクリックされました';
