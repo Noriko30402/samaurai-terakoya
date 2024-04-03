0d9374965904df0d199c1953171f00288c3e335f
Noriko30402 <151799807+Noriko30402@users.noreply.github.com>
2024-04-03 19:08:51 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,6 +0,0 @@
-const now = new Date();
-const year =now.getFullYear();
-const month = now.getMonth() + 1;
-const date = now.getDate();
-const formattedDate = year + "年" + month + "月" + date + "日";
-console.log(formattedDate);
